00934d8259fe4e8c6455c951d49c271c65c7ffc6
Allan le Couettos <55735176+CouetteAllan@users.noreply.github.com>
2023-01-05 09:46:19 +0100

    State Machine anim

---
+++ Assets/Boss_Fly.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Fly : StateMachineBehaviour
+{
+    public float speed = 5.0f;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+}
